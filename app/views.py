a5f0eefe24c2921d3d64da6a5d9d1b1a13f95097
trevor ngugi <ngugitrevor@gmail.com>
2020-01-17 15:59:05 +0300

    add dynamic route for blog

--- app/views.py
+++ app/views.py
@@ -8,3 +8,10 @@ def index():
     views the index page and its data
     """
     return render_template('index.html')
+
+@app.route('/blog/<user_id>')
+def blog(user_id):
+    """
+    views to show the authors blog
+    """
+    return render_template('blog.html',author=user_id)
