62bb091c243f43db5ad79c5894089b820d5ada9c
trevor ngugi <ngugitrevor@gmail.com>
2020-01-20 01:24:49 +0300

    initialised database ini init

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 bootstrap=Bootstrap()
+db=SQLAlchemy()
+
 def create_app(config_name):
     #initialising the app
     app=Flask(__name__)
@@ -13,7 +16,7 @@ def create_app(config_name):
 
     #intialising flask extensions
     bootstrap.init_app(app)
-
+    db.init_app(app)
     #reg blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY=os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2:/trevor:elite@localhost/dol'
     
 
 
