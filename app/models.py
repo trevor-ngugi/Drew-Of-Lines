632e63d74f40afea8c4b8d53b766764cfe65f36f
trevor ngugi <ngugitrevor@gmail.com>
2020-01-20 01:31:34 +0300

    add user model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
+from . import db
+
+class User(db.Model):
+    __tablename__='users'
+    id=db.column(db.Integer,primary_key=True)
+    username=db.column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+
+
+
+
+
+
+
+
+
+
 class Comment:
     all_comments=[]
 
@@ -12,3 +33,4 @@ class Comment:
     @classmethod
     def clear_reviews(cls):
         Comment.all_comments.clear()
+
