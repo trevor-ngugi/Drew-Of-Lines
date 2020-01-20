23103552f1f87500f8994a22d68cf10990fbb7aa
trevor ngugi <ngugitrevor@gmail.com>
2020-01-20 03:39:57 +0300

    add registration form class

--- app/models.py
+++ app/models.py
@@ -1,10 +1,13 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__='users'
     id=db.Column(db.Integer,primary_key = True)
-    username=db.Column(db.String(255))
+    username=db.Column(db.String(255),index=True)
+    email = db.Column(db.String(255),unique = True,index = True)
     pass_secure = db.Column(db.String(255))
     blog=db.relationship('Blog',backref='user',lazy='dynamic')
 
@@ -53,4 +56,8 @@ class Comment:
     @classmethod
     def clear_reviews(cls):
         Comment.all_comments.clear()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
